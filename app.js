647283f2f1a31d79b3165f64f129ce9eb29467c5
Mithilesh Das Aadithya <165498080+Mithi-18@users.noreply.github.com>
2024-09-19 16:53:28 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,43 @@
+let allImages = Array.from(document.querySelectorAll('.img_nav'));
+let popUp= document.querySelector('.pop_up');
+let cross= document.querySelector('.cross');
+let right= document.querySelector('.right');
+let popImage= document.querySelector('.preview');
+let currentPosition = 0;
+
+
+
+
+allImages.map((elmt, index) => {
+    elmt.addEventListener('click', ()=> {
+       currentPosition= index;
+       popImage.src= allImages[index].src;
+       popUp.classList.add('active');
+    
+       right.addEventListener('click',()=> {
+         if( currentPosition == (allImages.length)  -1){
+            currentPosition = 0;
+         }else{
+           currentPosition += 1;
+         }
+         popImage.src=allImages[currentPosition].src;
+         console.log(popImage.src);
+        
+
+        })
+         
+    })
+  });
+
+
+
+cross.addEventListener('click', ()=> {
+    popUp.classList.remove('active');
+});
+
+
+popUp.addEventListener('click',(e)=> {
+    if(e.target.classList.contains('active')){
+        popUp.classList.remove('active');
+    }
+});
